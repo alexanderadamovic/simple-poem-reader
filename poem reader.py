f00b330fc5ddf3613c0f2d311578f20a822c34d8
Alexander Adamovic <70420235+alexanderadamovic@users.noreply.github.com>
2026-03-19 01:03:10 +1100

    Add files via upload

---
+++ poem reader.py
@@ -0,0 +1,21 @@
+Line1 = input(f"Line 1: ")
+Line2 = input (f"Line 2: ")
+Line3 = input (f"Line 3: ")
+Line4 = input (f"Line 4: ")
+
+print(Line1)
+print(Line2)
+print(Line3)
+print(Line4)
+
+separators = int(input(f"Number of seperators: "))
+
+print('#' * separators)
+
+Line5 = input(f"Line 5: ")
+Line6 = input(f"Line 6: ")
+
+print(Line2)
+print(Line5)
+print(Line4)
+print(Line6)
